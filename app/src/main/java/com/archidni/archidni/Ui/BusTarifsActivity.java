a34af41dae45d1dce2ab0fcfb14b65407426def0
noureddine <nounoubensebia96@gmail.com>
2018-04-22 16:39:00 +0100

    adding path layout

---
+++ app/src/main/java/com/archidni/archidni/Ui/BusTarifsActivity.java
@@ -0,0 +1,13 @@
+package com.archidni.archidni.Ui;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BusTarifsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_bus_tarifs);
+    }
+}
